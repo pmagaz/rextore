41a65b97e8212fe8b4e472ffd2cde14f32dfa11d
Pablo Magaz <pmagaz@atsistemas.com>
2018-02-20 12:31:53 +0100

    Add ofType operator

--- src/operators/ofType.ts
+++ src/operators/ofType.ts
@@ -1,20 +1,38 @@
 import { Observable } from 'rxjs/Rx'
 
-export function ofType(predicate) {
+const ofTypeFn = (action, actionType, subscriber ) => {
+  try {
+    if (action.type == actionType) subscriber.next(action);
+    else subscriber.complete()
+  } catch (err) {
+    subscriber.error(err);
+  }
+}
+
+export function ofType(actionType) {
+  return function offTypeFn(source) {
+    return Observable.create(subscriber => {
+      let subscription = source.subscribe(value => {
+        ofTypeFn(value, actionType, subscriber)
+      },
+        err => subscriber.error(err),
+        () => subscriber.complete()
+      )
+      return subscription;
+   });
+  }
+}
+
+export function ofTypePrototype(actionType) {
   let source = this;
-  return Observable.create(observer =>
+  return Observable.create(subscriber =>
     source.subscribe(value => {
-      try {
-       if(value == predicate) observer.next(value);
-      } catch(e) {
-        observer.error(e);
-      }
+      ofTypeFn(value, actionType, subscriber)
     },
-    err => observer.error(err),
-    () => observer.complete()
+    err => subscriber.error(err),
+    () => subscriber.complete()
    )
-   return source
   );
 }
 
-//Observable.prototype.ofType = ofType 
+Observable.prototype.ofType = ofTypePrototype
